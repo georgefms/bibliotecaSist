5e437810197ca38d6a943afaaa5f51bcf83da039
George Felipe de Magalhães Silva <georgefelipe8@gmail.com>
2023-02-25 18:01:53 -0300

    Setup inicial do projeto
    arquivos gerados pelo spring

---
+++ src/main/java/br/com/georgefms/bibliotecaSist/BibliotecaSistApplication.java
@@ -0,0 +1,13 @@
+package br.com.georgefms.bibliotecaSist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliotecaSistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibliotecaSistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/georgefms/bibliotecaSist/BibliotecaSistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.georgefms.bibliotecaSist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotecaSistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

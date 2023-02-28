97ba43a07d97df3c1dce9877d3d33461cbefa93f
George Felipe de Magalhães Silva <georgefelipe8@gmail.com>
2023-02-27 22:09:45 -0300

    Implementando um post

--- src/main/java/br/com/georgefms/bibliotecaSist/controller/BooksController.java
+++ src/main/java/br/com/georgefms/bibliotecaSist/controller/BooksController.java
@@ -3,9 +3,9 @@ package br.com.georgefms.bibliotecaSist.controller;
 import br.com.georgefms.bibliotecaSist.model.Book;
 import br.com.georgefms.bibliotecaSist.repository.BookRepository;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -17,8 +17,16 @@ public class BooksController {
 
     private final BookRepository bookRepository;
 
+    //Listagem de todos os livros
     @GetMapping
     public List<Book> list(){
         return bookRepository.findAll();
+    }
+
+    //Post de um livro
+    @PostMapping
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public Book create(@RequestBody Book book){
+        return bookRepository.save(book);
     }
 }

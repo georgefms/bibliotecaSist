b7721ce423f016a5b7c0c625ca961785959b7a11
George Felipe de Magalhães Silva <georgefelipe8@gmail.com>
2023-03-14 23:02:13 -0300

    Atualizando modelo de usuário e repositório
    Necessário para implementações do security

--- src/main/java/br/com/georgefms/bibliotecaSist/model/User.java
+++ src/main/java/br/com/georgefms/bibliotecaSist/model/User.java
@@ -6,32 +6,57 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.UniqueElements;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
+import java.util.Collection;
 
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
-public class User {
+@Table(name = "tb_user")
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-    //@UniqueElements
     @NotBlank
     @Length(min =5, max= 30)
-    @Column(length = 30, nullable = false)
-    private String nick;
+    @Column(length = 30, nullable = false, unique = true)
+    private String username;
     @NotBlank
     @Length(min =8, max= 100)
     @Column(length = 100, nullable = false)
     private String password;
-    @NotBlank
-    //@Pattern(regexp = "Admin | User")
-    @Column(length = 5, nullable = false)
-    private String role = "User";
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }

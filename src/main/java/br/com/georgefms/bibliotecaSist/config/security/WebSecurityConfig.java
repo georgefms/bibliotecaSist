10ad6d3cd27101e7d9befedbfdc47f447414713b
George Felipe de Magalhães Silva <georgefelipe8@gmail.com>
2023-03-16 23:05:04 -0300

    explorando configs de rotas

--- src/main/java/br/com/georgefms/bibliotecaSist/config/security/WebSecurityConfig.java
+++ src/main/java/br/com/georgefms/bibliotecaSist/config/security/WebSecurityConfig.java
@@ -2,6 +2,7 @@ package br.com.georgefms.bibliotecaSist.config.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -14,14 +15,20 @@ import org.springframework.security.web.SecurityFilterChain;
  * @PreAuthorize("hasAnyHole('ROLE_ADMIN', ROLE_USER)") -> configuração de acesso para qualquer ROLE.
  * @PreAuthorize("hasHole('ROLE_ADMIN')") -> configuração de acesso para ROLE especifica.
  */
-@EnableGlobalMethodSecurity(prePostEnabled = true)
+//@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.httpBasic()
                 .and()
                 .authorizeHttpRequests()
-                //.antMatchers()
+                    .antMatchers(HttpMethod.GET, "/api/books/**").hasAnyRole("ROLE_ADMIN", "ROLE_USER")
+                    .antMatchers(HttpMethod.GET, "/api/users/**").hasRole("ROLE_ADMIN")
+                    .antMatchers(HttpMethod.POST, "/api/books/**").hasAnyAuthority("ROLE_ADMIN", "ROLE_USER")
+                    .antMatchers(HttpMethod.PUT, "/api/books/**").hasRole("ROLE_ADMMIN")
+                    .antMatchers(HttpMethod.DELETE, "/api/books/**").hasRole("ROLE_ADMMIN")
+                    //.antMatchers(HttpMethod.PUT, "/api/books/**").access("@")
+                    //.antMatchers(HttpMethod.DELETE, "/api/books/**").access()
                 .anyRequest().authenticated()
                 .and()
                 .csrf().disable();
